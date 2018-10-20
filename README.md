1ba57f3fe21e965b6194b02781a5cfb7bb18738e
Navraj Narula <navrajnarula@gmail.com>
2018-10-19 19:24:58 -0700

    add files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# substitute-medication
-This repository contains code I wrote to complete a technical challenge.
+### Substitute Medication
+##### Code written by Navie Narula
+
+This program outputs a file specifying appropriate medical substitutions that can be made for each perscription listed. To retrieve the file, simply type this command into your terminal once in the source directory: `python run.py`
+
+---
+
+>> Comments: I wrote a fairly simple function to complete the problem, and as instructed, spent a little less than an hour doing so. In actuality, a database structure might be necessary to avoid over-iteration so results can be returned via a simply query. In this case, looping over the apis to obtain the results were necessary because comparisons had to be made at each step. In the end, these results are stored in a hashmap structure so accessing elements does occur in constant time.
+
+
---
+++ src/run.py
@@ -0,0 +1,55 @@
+import requests
+import json
+
+def find_substitutes(pre, med):
+
+	# initialize dictionary to store results
+	updates = {}
+
+	# iterate through prescriptions
+	for p in pre: 
+
+		# iterate through medications
+		for m in med:
+
+			# check if medication id matches prescribed and generic value
+			if m["id"] == p["medication_id"] and m["generic"] == False:
+
+				for item in med:
+
+					# use rxcui value and genric value to find substitute for branded medication
+					if m["rxcui"] == item["rxcui"] and item["generic"] == True:
+
+						# perform update
+						updates[p["id"]] = item["id"]
+
+	# format results
+	updates = {"prescription_updates": [{"prescription_id": p, "medication_id": m} for p, m in updates.items()]}
+
+	return updates
+
+if __name__ == '__main__':
+
+	# connect to api
+	prescriptions = requests.get("http://api-sandbox.pillpack.com/prescriptions")
+	medications = requests.get("http://api-sandbox.pillpack.com/medications")
+
+	# store api content
+	pre = prescriptions.json()
+	med = medications.json()
+
+	# call function to find substitutes
+	results = find_substitutes(pre, med)
+
+	# write to file
+	with open('prescription_updates.json', 'w') as saved:
+		json.dump(results, saved, indent = 4)
+
+
+
+
+		
+
+
+
+
